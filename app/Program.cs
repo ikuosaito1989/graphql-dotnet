8e14a73a722cf5d99df0334638ec728379987234
ikuo saito <ikuosaito1989@gmail.com>
2020-12-24 17:10:41 +0900

    GraphType First

--- app/Program.cs
+++ app/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Types;
+using GraphQL.NewtonsoftJson;
 
-namespace app
+namespace App
 {
-    class Program
+    public class Droid
     {
-        static void Main(string[] args)
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class DroidType : ObjectGraphType<Droid>
+    {
+        public DroidType()
         {
-            Console.WriteLine("Hello World!");
+            Field(x => x.Id).Description("The Id of the Droid.");
+            Field(x => x.Name).Description("The name of the Droid.");
+        }
+    }
+
+    public class StarWarsQuery : ObjectGraphType
+    {
+        public StarWarsQuery()
+        {
+            Field<DroidType>(
+              "hero",
+              resolve: context => new Droid { Id = "1", Name = "R2-D2" }
+            );
+        }
+    }
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var schema = new Schema { Query = new StarWarsQuery() };
+
+            var json = await schema.ExecuteAsync(_ =>
+            {
+                _.Query = "{ hero { id name } }";
+            });
+
+            Console.WriteLine(json);
         }
     }
 }
